ffb8312b9903621eaf2095e3393b9fa14c7de193
Pavel_Klindziuk <pavel_klindziuk@epam.com>
2024-07-16 10:43:08 +0300

    update ai service to the latest version

--- src/main/java/com/klindziuk/ai/service/impl/LlamaAiServiceImpl.java
+++ src/main/java/com/klindziuk/ai/service/impl/LlamaAiServiceImpl.java
@@ -6,29 +6,30 @@ package com.klindziuk.ai.service.impl;
 
 import com.klindziuk.ai.model.LlamaResponse;
 import com.klindziuk.ai.service.LlamaAiService;
-import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LlamaAiServiceImpl implements LlamaAiService {
 
-  private final ChatClient chatClient;
+  private final OllamaChatModel ollamaChatModel;
 
   @Autowired
-  public LlamaAiServiceImpl(ChatClient chatClient) {
-    this.chatClient = chatClient;
+  public LlamaAiServiceImpl(OllamaChatModel ollamaChatModel) {
+    this.ollamaChatModel = ollamaChatModel;
   }
 
   @Override
   public LlamaResponse generateMessage(String promptMessage) {
-    final String llamaMessage = chatClient.call(promptMessage);
+    final String llamaMessage = ollamaChatModel.call(promptMessage);
     return new LlamaResponse().setMessage(llamaMessage);
   }
 
   @Override
   public LlamaResponse generateJoke(String topic) {
-    final String llamaMessage = chatClient.call(String.format("Tell me a joke about %s", topic));
+    final String llamaMessage =
+        ollamaChatModel.call(String.format("Tell me a joke about %s", topic));
     return new LlamaResponse().setMessage(llamaMessage);
   }
 }
